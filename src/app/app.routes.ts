e76e9332f7fe357509b7a3f23c18fcc1fe34ad8e
Estefania <tefajarita@gmail.com>
2018-05-22 22:56:16 -0500

    Cambios en el redireccionamiento de la aplicaciòn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {  Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 import { PagenofoundComponent } from './pagenofound/pagenofound.component';
 
 const appRoutes: Routes = [
     {path: 'login' , component: LoginComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'register' , component: RegisterComponent},
     {path: 'contact' , component: ContactComponent},
     {path: '' , redirectTo: '/404', pathMatch: 'full'},
